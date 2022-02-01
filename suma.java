9dd6592489958e106cad7681fd1bbc44d3fb9083
LoicH95 <loich995@gmail.com>
2022-02-01 13:20:23 +0000

    Añadimos suma y resta

---
+++ suma.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class suma{
+   
+   
+    static void menu(){
+        int a,b; 
+        int result;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Dame un numero: ");
+        a = sc.nextInt();
+
+        System.out.println("Dame un numero: ");
+        b = sc.nextInt();
+
+        result = a - b;
+
+        System.out.println(result);
+
+    }
+    public static void main(String[] arg){
+
+        menu();
+    } 
+}
